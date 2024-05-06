11aa5edab2c15de0553e9034fbc08312dc1147fd
DanielAlcantara1997 <gen.danielalcantara@gmail.com>
2024-05-06 09:47:32 +0800

    Added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express()
 
 const randomizerRoute = require('./server/routes/randomizer')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
